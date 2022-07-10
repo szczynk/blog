4c11dc469fc48b557f1fd47ff1e5af152c357c72
sanguinary-bus <sanguinary-bus@users.noreply.github.com>
2022-07-10 19:24:45 +0000

    deploy: 84d67463c94b64aa9536e825ebf4df862fbc1bf1

---
+++ _nuxt/c118ba5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{329:function(t,n,e){"use strict";e.r(n);var c={},l=e(6),component=Object(l.a)(c,(function(){var t=this._self._c;return t("section",{},[t("div",{staticClass:"px-4 py-6 container"},[this._t("default")],2)])}),[],!1,null,null,null);n.default=component.exports}}]);
