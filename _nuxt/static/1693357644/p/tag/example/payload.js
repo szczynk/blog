aa45035c9d4f5e8b8154442e1bc3f4dfb7885b28
szczynk <szczynk@users.noreply.github.com>
2023-09-01 03:47:50 +0000

    deploy: ba7a16aeb8a91a94a5c289e8abc5214be3856187

--- _nuxt/static/1693357644/p/tag/example/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/p/tag/example", (function(a,b){return {data:[{tags:[{slug:b,title:b,subtitle:a,cover:{image:a,thumb:a,alt:a,caption:a},createdAt:"2023-08-30T01:06:24.050Z",tags:[b]}]}],fetch:{},mutations:void 0}}("","example")));
